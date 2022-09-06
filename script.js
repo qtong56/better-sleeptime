5f83543b437b2ab88b5caca66fcc4f464b657207
Qilin Tong <qilintong56@gmail.com>
2022-09-06 12:33:54 -0400

    calculation script from user input

--- script.js
+++ script.js
@@ -14,9 +14,16 @@ function handleOnClick() {
     let hours = document.getElementById("hours");
     hours.innerHTML = "";
   
+    let hh = document.getElementById("hh").value;
+    let mm = document.getElementById("mm").value;
+    let ampm = document.getElementById("ampm").value;
+    hh = ampm === "PM" ? Number.parseInt(hh) + 12 : hh;
+    console.log({hh, mm, ampm});
+
     let now = new Date();
     // allow 14 minutes to fall sleep!
-    now.setMinutes(now.getMinutes() + 14);
+    now.setHours(hh)
+    now.setMinutes(mm);
   
     // calculate sleep cycles!
     for (let i = 1; i <= 6; i++) {
